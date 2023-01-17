e7c5c28a0b92f1c0a8778932e9c2fc918c7a2a30
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-17 17:02:55 +0100

    Created ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
+
   _HomePageState();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
 
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text(
+          "Taskly!",
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: _tasksList(),
     );
   }
 
+  Widget _tasksList() {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text(
+            "Get Groceries",
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          subtitle: Text(DateTime.now().toString()),
+          trailing: const Icon(
+            Icons.check_box_outlined,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
+  }
 }
