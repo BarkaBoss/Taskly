e00c866daaa1d75edd92995d0e565dcedf54d530
BarkaBoss <bossmajor.biii@gmail.com>
2023-01-17 20:10:20 +0100

    initialized Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:taskly/pages/home_page.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter("hive_boxes");
   runApp(const MyApp());
 }
 
